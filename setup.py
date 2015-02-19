d7ff3ccf44e3c983b7fd4a089e4863dccc9802dc
amandapickering <pickering.amanda@gmail.com>
2015-02-19 18:05:21 -0500

    updated version for pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
   name = 'markovgenerator',
-  version = '0.0.4',
+  version = '0.0.5',
   description = 'Markov text generator',
   license = 'MIT',
   author = 'Amanda Pickering',
