b994c21a4d0704902cbbc67de0f69a900017490d
Cathal Garvey <cathalgarvey@cathalgarvey.me>
2015-07-04 20:59:46 +0100

    Removed wsgi from setup.py, not sure what role it was serving

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     license = 'MIT',
     author = 'Amanda Pickering',
     author_email = 'pickering.amanda@gmail.com',
-    install_requires = ['nltk', 'wsgiref'],
+    install_requires = ['nltk'],  # Removed 'wsgiref', didn't appear to be used/needed?
     url = 'https://github.com/amandapickering/markovgenerator',
     keywords = 'markov text generator natural language processing generative',
     packages = find_packages(),
